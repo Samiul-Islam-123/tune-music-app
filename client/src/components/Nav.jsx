b7ef338f8207f337c762e7e5baa1e2e281e32d22
Sourish Samanta <sourishsamanta1234@gmail.com>
2024-07-06 03:33:56 +0530

    player added

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,6 +4,7 @@ import { CiSearch } from "react-icons/ci";
 import { UserButton } from '@clerk/clerk-react';
 import axios from "axios"
 import SongCard from './SongCard';
+import SearchSongCard from './SearchSongCard';
 
 function Nav({ click, setClick }) {
 
@@ -38,17 +39,18 @@ function Nav({ click, setClick }) {
   return (
     <>
       <header className=' z-10'>
-        <nav className=' w-screen h-[7vh]  flex justify-between items-center px-8 border'>
+        <nav className=' bg-zinc-800 bg-opacity-50 backdrop-filter backdrop-blur-lg  border-white/20 rounded-lg flex-1 h-[7vh] border-b flex justify-between items-center px-8  mx-3 '>
           <div className=' w-[5vh] h-[5vh] rounded-full hover:border  flex justify-center items-center'>
             <TbMenu2 onClick={() => {
               setClick(!click)
             }} size={20} />
           </div>
-          <div className='  w-1/4 h-[5vh] border rounded-full px-4 flex items-center'>
+          <div className=' w-1/4 h-[5vh] border rounded-full px-4 flex items-center'>
             <input type="text" onChange={e => {
               setSearchQeury(e.target.value);
             }} className=' flex flex-1 focus:outline-none h-full' />
             <CiSearch size={24} className=' ml-auto ' />
+            
           </div>
           <div className='flex items-center justify-center w-[5vh] h-[5vh] rounded-full ' style={{ width: '34px', height: '34px' }}>
             <UserButton />
@@ -56,17 +58,16 @@ function Nav({ click, setClick }) {
         </nav>
       </header>
       {SearchResults && (<>
-        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
+        <div className='bg-zinc-800 bg-opacity-80 backdrop-filter backdrop-blur-lg  border-white/20 z-50 fixed top-[7vh] left-[26vw] grid grid-cols-2 grid  w-2/4 h-[60vh]'>
 
         {
           SearchResults.map(item => {
             console.log(item)
             return (<>
-              <SongCard
+              <SearchSongCard
                 key={item.id}
                 image={item.thumnailURL}
                 title={item.SongTitle}
-                setCurrentSong={setCurrentSong}
                 Song={item}
                 />
 
@@ -75,6 +76,7 @@ function Nav({ click, setClick }) {
         }
         </div>
       </>)}
+      
 
 
     </>
--- client/src/components/Playheader.jsx
+++ client/src/components/Playheader.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import { useSong } from "../contexts/SongContext";
+import Player from "./Player";
 
 function Playheader({ playNextSong }) {
 
@@ -18,7 +19,7 @@ function Playheader({ playNextSong }) {
     return (
         <>
             {currentSong && (
-                <div className="z-10 fixed bottom-0 w-full h-[13vh] border flex flex-1 items-center px-5">
+                <div className="z-10 fixed bottom-0 w-full border-t-white/30 border-t h-[13vh] bg-zinc-900 bg-opacity-40 rounded-lg flex flex-1 items-center px-5">
                     <div className="w-[9vh] h-[9vh] rounded-xl border mr-[3.5vw]">
                         {currentSong.thumnailURL && (
                             <img
@@ -28,13 +29,14 @@ function Playheader({ playNextSong }) {
                             />
                         )}
                     </div>
-                    <div className="flex flex-col mr-auto">
+                    <div className="flex flex-col ">
                         <h3 className="text-lg font-bold">{currentSong.SongTitle}</h3>
                         <a href={currentSong.SongURL} className="text-sm text-blue-500" target="_blank" rel="noopener noreferrer">
                             Listen to the song
                         </a>
                     </div>
                     <div className='flex items-center'>
+                        <Player />
                         <audio
                             ref={audioRef}
                             controls
